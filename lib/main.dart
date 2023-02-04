72ca3cbb38d54694572f9427cdabcb1707727b46
Riwaj <neupaneriwaj64@gmail.com>
2023-02-04 14:04:02 +0545

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
       initialRoute:'a',
       routes: {
         'a': (context) => MyHomePage(),
